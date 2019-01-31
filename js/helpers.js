af70dce8ca367597ecda6d64baae765dbcc2a839
vinnyA3 <vincent.aceto@gmail.com>
2019-01-31 00:29:27 -0500

    feat(helpers): implement checkWinner functionality

--- js/helpers.js
+++ js/helpers.js
@@ -1,82 +1,97 @@
 (function(window) {
-  const checkForWinningRow = (board, currentPlayer) => {
-    let xCount = 0;
-    let oCount = 0;
-
+  const checkForWinningRow = ({ board, currentPlayer }) => {
     for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[0].length; j++) {
-        if (board[i][j] === 'X') {
-          console.log(xCount);
+      let xCount = (oCount = 0);
+
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === '') {
+          break;
+        } else if (board[i][j] === 'X') {
           xCount++;
         } else if (board[i][j] === 'O') {
           oCount++;
         }
       }
-    }
 
-    console.log('x count: ', xCount);
-    console.log('o count: ', oCount);
-
-    if (xCount === 3 || oCount === 3) {
-      console.log('row');
-      alert(`currentPlayer: ${currentPlayer ? 'X' : 'O'} wins!!`);
-      return;
+      if (xCount === 3 || oCount === 3) {
+        return true;
+      }
     }
   };
 
-  const checkForWinningMajor = (board, currentPlayer) => {
-    let xCount = 0;
-    let oCount = 0;
+  const checkForWinningColumn = ({ board, currentPlayer }) => {
+    let rowCount = (xCount = oCount = 0);
 
-    for (let i = 0; i < board.length - 1; i++) {
-      const current = board[i + 1][i + 1] || '';
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[0].length; j++) {
+        let current = board[j][i];
+
+        if (current !== '') {
+          if (current === 'X') {
+            xCount++;
+          } else if (current === 'O') {
+            oCount++;
+          }
+        }
+
+        if (xCount === 3 || oCount === 3) {
+          return true;
+        }
+      }
+
+      xCount = oCount = 0;
+    }
+  };
+
+  const checkForWinningMajor = ({ board, currentPlayer }) => {
+    let xCount = (oCount = colCount = 0);
+
+    for (let i = 0; i < board.length; i++) {
+      const current = board[i][colCount++] || '';
 
       if (current !== '') {
         if (current === 'X') {
           xCount++;
-        } else if (current === 'Y') {
+        } else if (current === 'O') {
           oCount++;
         }
       }
     }
 
     if (xCount === 3 || oCount === 3) {
-      console.log('major');
-      alert(`currentPlayer: ${currentPlayer ? 'X' : 'O'} wins!!`);
-      return;
+      return true;
     }
   };
 
-  const checkForWinningMinor = (board, currentPlayer) => {
-    let xCount = 0;
-    let oCount = 0;
+  const checkForWinningMinor = ({ board, currentPlayer }) => {
+    let xCount = (oCount = 0);
+    let colCount = board.length - 1;
 
-    for (let i = board.length - 1; i >= 0; i--) {
-      const current = board[i][i - 1] || '';
+    for (let i = 0; i < board.length; i++) {
+      const current = board[i][colCount--] || '';
 
       if (current !== '') {
         if (current === 'X') {
           xCount++;
-        } else if (current === 'Y') {
+        } else if (current === 'O') {
           oCount++;
         }
       }
     }
 
     if (xCount === 3 || oCount === 3) {
-      console.log('minor');
-      alert(`currentPlayer: ${currentPlayer} wins!!`);
-      return;
+      return true;
     }
   };
 
-  const checkForWinner = ({ board, currentPlayer }) => {
-    checkForWinningRow(board, currentPlayer);
-    checkForWinningMajor(board, currentPlayer);
-    checkForWinningMinor(board, currentPlayer);
-  };
+  const weHaveAwinner = model =>
+    checkForWinningRow(model) ||
+    checkForWinningColumn(model) ||
+    checkForWinningMajor(model) ||
+    checkForWinningMinor(model) ||
+    false;
 
   window.helpers = {
-    checkForWinner,
+    weHaveAwinner,
   };
 })(window);
